92e84a32aecb74fa790a77e24bc34014f29d67f7
ayush20021 <84669540+ayush20021@users.noreply.github.com>
2024-01-19 00:48:01 +0530

    *

---
+++ Object.js
@@ -0,0 +1,30 @@
+
+
+
+const a = { 
+    name:"Ayush",
+    age:21,
+    hobbies:["Reading","Coding","Sleeping"]
+}
+
+console.log(a)
+const b = Object.values(a);
+console.log(b)
+
+class Employee{
+    constructor(Name, Age){
+        this.Name = Name;
+        this.Age = Age;
+    }
+}
+
+const Employee1 = new Employee("Ayush", 21);
+const Employee2 = new Employee("Ayush", 21);
+const Employee3 = new Employee("Ayush", 21);
+const Employee4 = new Employee("Ayush", 21);
+
+console.log(Employee1.Name);
+
+const  c= Object.values(Employee1);
+
+console.log(c);
