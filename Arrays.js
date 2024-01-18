314095975cebc264d3991b8b1c6be44ce71f3489
ayush20021 <84669540+ayush20021@users.noreply.github.com>
2024-01-18 22:42:13 +0530

    *

--- Arrays.js
+++ Arrays.js
@@ -1,4 +1,4 @@
-let a = [1, 2, 3, 4, 5, 6, 7, 8,"Ayush", "Bat", "Cat", "Dog"];
+let a = [1, 2, 3, 4, 5, 6, 7, 8];
 
 // Adding
 a.push("Frog"); // Add the  element to the end of the array
@@ -11,3 +11,26 @@ a.pop(); // Remove the last element of the array
 a.shift(); // Remove the first element of the array
 
 console.log(a);
+
+// Map functionality
+let b = a.map(function (a) { 
+                                 // Advanced for loop iteration
+    return  a*5;
+
+})
+
+console.log(b);
+
+// Filter functionality
+let  c = a.filter(function (a) { 
+    return a%2==0;             // Type of if else condition
+});
+
+console.log(c);
+
+// Reduce functionality
+
+let d = a.reduce(function(a,b) {
+    return a+=b;                  // Add the element to the end of the array
+})
+console.log(typeof (d));
