3c6af558723df9c85519412363af37b13d93a759
ayush20021 <84669540+ayush20021@users.noreply.github.com>
2024-01-19 00:32:03 +0530

    *

--- Arrays.js
+++ Arrays.js
@@ -34,3 +34,24 @@ let d = a.reduce(function(a,b) {
     return a+=b;                  // Add the element to the end of the array
 })
 console.log(typeof (d));
+
+// Splice functionality 
+
+const index = 2;
+const element = 55
+                                // To update the specific element in the array
+a.splice(index,1,element);
+
+console.log(a);
+
+
+const d1 = 3;                   // To update the specific element in the array
+const d2 = "ABC";
+a.splice(d1,0,d2);
+
+
+const e1 = 3;
+const e2 = 4;                   //To  remove elemeny from an array
+a.splice(e1,e2);
+
+
