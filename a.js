a8f24f034c69437e0b4ad7c05825136189372db5
ayush20021 <84669540+ayush20021@users.noreply.github.com>
2024-01-18 18:28:18 +0530

    Learn  JAvaScript

--- a.js
+++ a.js
@@ -1,7 +1,9 @@
-// var a = 10;
+var a = 10;
 
-// console.log(typeof(a));
-//  a = 10.0121469;
-// //  console.log(a);
-//  console.log(typeof(a));
+console.log(typeof(a));
+ a = 10.0121469;
+//  console.log(a);
+ console.log(typeof(a));
+
+
 
